534a5fecb6c408a17c810527b5c3ec780a3f4eb7
jinzhengang <jinzhengang@didichuxing.com>
2021-02-10 16:20:38 +0800

    LRU

---
+++ src/main/java/com/jzg/leetcode/lru/LinkedHashMapLru.java
@@ -0,0 +1,49 @@
+package com.jzg.leetcode.lru;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+/**
+ * 基于LinkedHashMap实现LRU
+ *
+ * @author jinzg
+ * @date 2021/2/10
+ */
+public class LinkedHashMapLru {
+
+  private LinkedHashMap<Integer, Object> cache;
+  private int capacity;
+
+  public LinkedHashMapLru(int capacity) {
+    cache = new LinkedHashMap();
+    this.capacity = capacity;
+  }
+
+  public Object get(int key) {
+    if (cache.containsKey(key)) {
+      Object val = cache.get(key);
+      // 从链表中删除
+      cache.remove(key);
+      // 添加到链尾
+      cache.put(key, val);
+    }
+    return null;
+  }
+
+  public void put(int key, Object value) {
+    if (cache.containsKey(key)) {
+      // 从链表中删除
+      cache.remove(key);
+    }
+    if (cache.size() == capacity) {
+      Set<Integer> keySet = cache.keySet();
+      Iterator<Integer> iterator = keySet.iterator();
+      // 删除链头
+      cache.remove(iterator.next());
+    }
+    // 添加到链尾
+    cache.put(key, value);
+  }
+
+}
---
+++ src/main/java/com/jzg/leetcode/lru/LinkedListLru.java
@@ -0,0 +1,54 @@
+package com.jzg.leetcode.lru;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
+/**
+ * 基于LinkedList实现LRU
+ *
+ * @author jinzg
+ * @date 2021/2/10
+ */
+public class LinkedListLru<T> {
+
+  private LinkedList<T> cache;
+  private int capacity;
+
+  public LinkedListLru(int capacity) {
+    cache = new LinkedList<>();
+    this.capacity = capacity;
+  }
+
+  public Object get(T target) {
+    Iterator<T> iterator = cache.descendingIterator();
+    while (iterator.hasNext()) {
+      T t = iterator.next();
+      if (Objects.equals(t, target)) {
+        // 删除并添加到链表尾部
+        iterator.remove();
+        put(target);
+        return t;
+      }
+    }
+    return null;
+  }
+
+  public void put(T target) {
+    Iterator<T> iterator = cache.descendingIterator();
+    while (iterator.hasNext()) {
+      T t = iterator.next();
+      if (Objects.equals(t, target)) {
+        // 删除
+        iterator.remove();
+      }
+    }
+    if (cache.size() == capacity) {
+      // 删除链头
+      cache.removeFirst();
+    }
+    // 添加到链表尾部
+    cache.add(target);
+  }
+
+}
