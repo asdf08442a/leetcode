ec23699c173c063687ed3be27d1ecfdc2dba14f3
jinzhengang <jinzhengang@didichuxing.com>
2021-02-12 15:51:55 +0800

    TreeNode

---
+++ src/main/java/com/jzg/leetcode/treeNode/IsAVL.java
@@ -0,0 +1,45 @@
+package com.jzg.leetcode.treeNode;
+
+/**
+ * 第k层节点数
+ *
+ * @author jinzg
+ * @date 2021/2/12
+ */
+public class IsAVL {
+
+  public static void main(String[] args) {
+
+    TreeNode root2111 = new TreeNode(null, null, 8);
+
+    TreeNode root211 = new TreeNode(root2111, null, 4);
+    TreeNode root212 = new TreeNode(null, null, 5);
+    TreeNode root21 = new TreeNode(root211, root212, 2);
+
+    TreeNode root221 = new TreeNode(null, null, 6);
+    TreeNode root222 = new TreeNode(null, null, 7);
+    TreeNode root22 = new TreeNode(root221, root222, 3);
+
+    TreeNode root = new TreeNode(root21, root22, 1);
+
+    System.out.println(isAVLRecur(root));
+  }
+
+  static boolean isAVLRecur(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
+      return false;
+    }
+    return isAVLRecur(root.left) && isAVLRecur(root.right);
+  }
+
+  static int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
+
+}
