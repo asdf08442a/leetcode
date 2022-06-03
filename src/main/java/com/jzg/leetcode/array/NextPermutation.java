2eebdb41f3f3fcdaa7fc46c5dbffe3f8e55d0c12
asdf08442a <18330068303@139.com>
2022-06-03 09:55:30 +0800

    add

---
+++ src/main/java/com/jzg/leetcode/array/NextPermutation.java
@@ -0,0 +1,44 @@
+package com.jzg.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 下一个排列
+ */
+public class NextPermutation {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 5, 1};
+        nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
+    }
+
+    private static void reverse(int[] nums, int i) {
+        int left = i, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
---
+++ src/main/java/com/jzg/leetcode/array/SearchRange.java
@@ -0,0 +1,73 @@
+package com.jzg.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 在排序数组中查找元素的第一个和最后一个位置
+ */
+public class SearchRange {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1};
+        System.out.println(Arrays.toString(searchRange1(nums, 1)));
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+        int leftIndex = binarySearch(nums, target, true);
+        int rightIndex = binarySearch(nums, target, false) - 1;
+        if (leftIndex <= rightIndex && rightIndex < nums.length && nums[leftIndex] == target && nums[rightIndex] == target) {
+            return new int[]{leftIndex, rightIndex};
+        }
+        return new int[]{-1, -1};
+    }
+
+    private static int binarySearch(int[] nums, int target, boolean flag) {
+        int l = 0, r = nums.length - 1, ans = nums.length;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > target || (flag && nums[mid] >= target)) {
+                r = mid - 1;
+                ans = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static int[] searchRange1(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        if (nums.length == 1 && nums[0] == target) {
+            return new int[]{0, 0};
+        }
+
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) /2;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        if (nums[l] != target) {
+            return new int[]{-1, -1};
+        }
+        int left = l;
+
+        l = 0;
+        r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) /2;
+            if (nums[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return new int[]{left, l - 1};
+    }
+}
